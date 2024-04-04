2f203111da7b63bce75a324d7a1474f74550ece7
Huntertk <taufik.khan65@gmail.com>
2024-04-04 11:51:55 +0530

    Favicon and Logo Changed For malaysia experience

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import '../styles/header.scss';
-import logo from '../assets/images/tvagologo.png';
+import logo from '../assets/images/logo.png';
 import { FaAlignRight } from "react-icons/fa6";
 import { useState } from 'react';
 import SmallNavbar from './SmallNavbar';
