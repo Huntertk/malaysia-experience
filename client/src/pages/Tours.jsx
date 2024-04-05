14af8d02437970f830cc9a85a81b805dc3b77d52
Huntertk <taufik.khan65@gmail.com>
2024-04-05 15:53:21 +0530

    Tour Dynamic Name Added

--- client/src/pages/Tours.jsx
+++ client/src/pages/Tours.jsx
@@ -1,12 +1,18 @@
 import '../styles/tours.scss';
-import { ToursGrid } from '../components'
-import { availableToursData } from '../data'
-
+import { ToursGrid } from '../components';
+import { availableToursData } from '../data';
+import {Helmet} from "react-helmet";
 const Tours = () => {
   return (
-    <section className='toursMainSection'>
-      <ToursGrid data={availableToursData} title={"Tours Availabe"} />
-    </section>
+    <>
+     {/*Dynamic Title */}
+     <Helmet>
+        <title>Malaysia Experience - Tours | Best Deals & Discounts</title>
+      </Helmet>
+      <section className='toursMainSection'>
+        <ToursGrid data={availableToursData} title={"Tours Availabe"} />
+      </section>
+    </>
   )
 }
 
